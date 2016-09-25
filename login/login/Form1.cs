6d361743f3100edbab3acbe304b9693d80254985
pelpose <pelpose@gmail.com>
2016-09-25 13:08:04 +1300

    Add files via upload
    test

---
+++ login/login/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text =="admin")
+            {
+                if(textBox2.Text == "123")
+                {
+                    MessageBox.Show(textBox1.Text + ",Hello~!");
+                }
+                else
+                {
+                    MessageBox.Show("Your Password is incorrect");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Your ID is incorrect");
+            }
+            
+        }
+    }
+}
